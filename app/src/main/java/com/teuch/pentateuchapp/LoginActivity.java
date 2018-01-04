de5e648c8022430f8540540ef43c943c01b8d385
Md.Saif <mds71964@gmail.com>
2018-01-04 12:32:57 +0530

    hey ...

--- app/src/main/java/com/teuch/pentateuchapp/LoginActivity.java
+++ app/src/main/java/com/teuch/pentateuchapp/LoginActivity.java
@@ -2,12 +2,28 @@ package com.teuch.pentateuchapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    ImageView logo;
+    TextView textView;
+    Animation fromtop,frombottom;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        textView=(TextView)findViewById(R.id.text) ;
+        logo=(ImageView)findViewById(R.id.logo);
+        fromtop= AnimationUtils.loadAnimation(this,R.anim.fromtop);
+        logo.setAnimation(fromtop);
+        frombottom=AnimationUtils.loadAnimation(this,R.anim.frombottom);
+        textView.setAnimation(frombottom);
+
     }
 }
